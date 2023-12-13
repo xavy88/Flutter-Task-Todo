9926c2bf32994a087ecef5a6a5ea78dfbac2cf65
xavy88 <xavyurbina88@gmail.com>
2023-12-13 16:57:19 -0600

    Repository for CRUD functionality implemented

---
+++ lib/data/repositories/task_repository.dart
@@ -0,0 +1,8 @@
+import 'package:todo_app/data/data.dart';
+
+abstract class TaskRepository {
+  Future<void> createTask(Task task);
+  Future<void> updateTask(Task task);
+  Future<void> deleteTask(Task task);
+  Future<List<Task>> getAllTasks();
+}
